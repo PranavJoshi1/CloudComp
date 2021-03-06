2a95a715958b9564e223723245a0432b30fe92eb
Pranav Chandrashekhar Joshi <2019pcs0022@iitjammu.ac.in>
2021-03-06 14:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,3 @@ def fahrenheit_from(celsius):
     fahrenheit = float(celsius) * 9 / 5 + 32
     fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
     return str(fahrenheit)
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
