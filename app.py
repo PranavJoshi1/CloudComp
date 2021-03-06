90b21977edd1ae71d341aa4dfedaef7e3517fce7
Pranav Chandrashekhar Joshi <2019pcs0022@iitjammu.ac.in>
2021-03-06 15:01:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
-#from flask import Flask
-#app = Flask(__name__)
-
-#@app.route("/")
-#def hello():
-#    return "Hello, World!
-
-
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    fahrenheit = float(celsius) * 9 / 5 + 32
-    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-    return str(fahrenheit)
+def hello():
+   return "Hello, World!
